adacd228c3a84fd1058dd1a3dd536700344198b3
SYLVIE <SYLVIE@SYLVIE-PC>
2018-11-15 00:15:30 +0100

    Template

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -6,17 +6,22 @@ use App\Entity\Reservations;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('num_reservation')
-            ->add('date_visite')
-            ->add('date_reservation')
-            ->add('nb_billets')
-            ->add('mail')
+                ->add('num_reservation')
+                ->add('date_visite', DateTimeType::class, array(
+                    'widget' => 'choice',
+                    'format' => 'dd-MM-yyyy HH:mm'))
+                ->add('date_reservation', DateTimeType::class, array(
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy HH:mm'))
+                ->add('nb_billets')
+                ->add('mail')
         ;
     }
 
