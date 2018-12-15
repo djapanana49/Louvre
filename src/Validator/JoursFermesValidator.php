89e4a9cc700f87f34eb56f97d2f1f5797f11f92c
SYLVIE <SYLVIE@SYLVIE-PC>
2018-12-15 22:40:02 +0100

    jours_feries

--- src/Validator/JoursFermesValidator.php
+++ src/Validator/JoursFermesValidator.php
@@ -4,19 +4,41 @@ namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use DateInterval;
 
 class JoursFermesValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
         /* @var $constraint App\Validator\JoursFermes */
-        var_dump($value->format('d/m'));
-        $jours_feries=new \DateTime();
-        $jours_feries->format('d/m');
-       $a=array("25/12","01/01","01/05","08/05","14/07","15/08","01/11","11/11");
-if (array_key_exists($value->format('d/m'),$a))
+       $annee_actuelle=$value->format('Y');
+       $interval = new DateInterval('P38D');
+       $interval2 = new DateInterval('P11D');
+       $jours=(easter_days($annee_actuelle));
+       $datedeb = new \DateTime($annee_actuelle.'-03-21');
+       $paques = $datedeb;
+       $paques = $paques->add(new DateInterval('P'.$jours.'D'));
+       $LundiPaques=$paques;
+       $LundiPaques=$LundiPaques->add(new DateInterval('P1D'));
+       $ascension=$paques;
+       $ascension=$ascension->add($interval);
+       $pentecote=$paques;
+       $pentecote=$pentecote->add($interval2);
+       
+       
+        
+        
+        
+
+       $a=array("25/12","01/01","01/05","08/05","14/07","15/08","01/11","11/11",$paques->format('d/m'),$LundiPaques->format('d/m'),$ascension->format('d/m'),$pentecote->format('d/m'));
+       var_dump($a);
+       $jd2=cal_to_jd(CAL_GREGORIAN,date("m"),date("d"),date("Y"));
+
+echo(jddayofweek($jd2,0));
+       die;
+if (in_array($value->format('d/m'),$a))
   {
-   var_dump($a);die;
+   
   $this->context->buildViolation($constraint->message)
             ->setParameter('{{ value }}', $value->format('d/m'))
             ->addViolation();
